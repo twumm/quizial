fa3fa10375f51be3f0296a50d5d61ed2b021479f
twumm <martint.mensah@gmail.com>
2018-06-09 22:39:18 +0000

    feat: Define GET and POST requests using the controller.

--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const question_controller = require('../controllers/questionController');
 
 /// QUESTION ROUTES ///
 
 // GET request to create a Question.
-router.get('/create', );
+router.get('/question/create', question_controller.question_create_get);
+
+// POST request to create a Question.
+router.post('/question/create', question_controller.question_create_post);
+
+// GET request to update a Question.
+router.get('/question/:id/update', question_controller.question_update_get);
+
+// POST request to update a Question.
+router.post('/question/:id/update', question_controller.question_update_post);
+
+// GET request to delete a Question.
+router.get('/question/:id/delete', question_controller.question_delete_get);
+
+// POST request to update a Question.
+router.post('/question/:id/delete', question_controller.question_delete_post);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const user_controller = require('../controllers/userController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// GET request to create a User.
+router.get('/user/create', user_controller.user_create_get);
+
+// POST request to create a User.
+router.post('/user/create', user_controller.user_create_post);
+
+// GET request to update a User.
+router.get('/user/:id/update', user_controller.user_update_get);
+
+// POST request to update a User.
+router.post('/user/:id/update', user_controller.user_update_post);
+
+// GET request to delete a User.
+router.get('/user/:id/delete', user_controller.user_delete_get);
+
+// POST request to delete a User.
+router.post('/user/:id/delete', user_controller.user_delete_post);
 
 module.exports = router;
