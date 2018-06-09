17f31aa78a12407e05bbcc651fdff71054d532c7
twumm <martint.mensah@gmail.com>
2018-06-09 20:59:54 +0000

    feat: Set app.use on quiz.

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const quiz = require('./routes/quiz');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/questions', users);
+app.use('/quiz', quiz);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
