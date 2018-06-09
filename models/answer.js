999e86d184ff0382d33ba28db77a804a08ae78ef
twumm <martint.mensah@gmail.com>
2018-06-09 20:15:31 +0000

    refactor: Add answer options as array.

--- models/answer.js
+++ models/answer.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AnswerSchema = new Schema({
-  answer: { type: String, max: 300, required: true },
+  answerCorrect: { type: String, max: 300, required: true },
   answerOption: [{ type: String, max: 300, required: true }],
   question: { type: Schema.ObjectId, ref: 'Question', required: true },
 });
