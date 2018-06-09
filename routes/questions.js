94a39432a5cc50e8220286a4c1a347d6465d04ca
twumm <martint.mensah@gmail.com>
2018-06-09 21:00:55 +0000

    feat: Set up router for questions.

---
+++ routes/questions.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/// QUESTION ROUTES ///
+
+// GET request to create a Question.
+router.get('/create', );
+
+module.exports = router;
