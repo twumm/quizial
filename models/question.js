0d9ae35246946433803041dcdb57fb88d09e1bc7
twumm <martint.mensah@gmail.com>
2018-06-09 20:17:35 +0000

    refactor: Add usersCorrect(number of users who answered correct) and submittedBy.

--- models/question.js
+++ models/question.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
   question: { type: String, max: 1000, required: true },
-  answer: [{ type: Schema.ObjectId, ref: 'Answer', required: true }],
-  usersAnsweredCorect: [{ type: Schema.ObjectId, ref: 'User' }],
+  usersCorrect: [{ type: Schema.ObjectId, ref: 'User' }],
+  usersAttempted: [{ type: Schema.ObjectId, ref: 'User' }],
   submittedBy: { type: Schema.ObjectId, ref: 'User' },
-  seenCount: { type: Number, default: 0 },
-  correctCount: { type: Number, default: 0 },
-  wrongCount: { type: Number, default: 0 },
   dateCreated: { type: Date, default: Date.now },
   dateUpdated: { type: Date, default: Date.now }
+  // seenCount: { type: Number, default: 0 },
+  // correctCount: { type: Number, default: 0 },
+  // wrongCount: { type: Number, default: 0 },
 });
