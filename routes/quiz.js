eabc29e4c3712d02b415dfd897ea6483ff5f6b12
twumm <martint.mensah@gmail.com>
2018-06-10 07:26:31 +0000

    feat: Set route and path to display/handle quiz on GET or POST request.

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const quiz_controller = require('../controllers/quizController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// GET request to display quiz.
+router.get('/:id', quiz_controller.quiz_display_get);
+
+// POST request to display quiz.
+router.post('/:id', quiz_controller.quiz_display_post);
 
 module.exports = router;
