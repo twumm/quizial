79895f8d0d8c4de76e708f0f2d201c75fa67d75f
twumm <martint.mensah@gmail.com>
2018-06-10 07:25:22 +0000

    feat: Set up function for displaying quiz on GET and POST requests.

---
+++ controllers/quizController.js
@@ -0,0 +1,12 @@
+const Question = require('../models/question');
+const User = require('../models/user');
+const Answer = require('../models/answer');
+
+// Display quiz on GET.
+exports.quiz_display_get = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: Quiz display get');
+}
+
+exports.quiz_display_post = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: Quiz display post');
+}
