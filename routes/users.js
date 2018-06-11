3a7889b59d46347a244084edce1e02efdca18074
twumm <martint.mensah@gmail.com>
2018-06-11 14:55:50 +0000

    feat: Add signin and signup route for GET and POST.

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const user_controller = require('../controllers/userController');
 
 
-// GET request to create a User.
-router.get('/user/create', user_controller.user_create_get);
+// GET request to signup a User.
+router.get('/user/signup', user_controller.user_signup_get);
 
-// POST request to create a User.
-router.post('/user/create', user_controller.user_create_post);
+// POST request to signup a User.
+router.post('/user/signup', user_controller.user_signup_post);
+
+// GET request to signin a User.
+router.get('/user/signin', user_controller.user_signin_get);
+
+// POST request to signin a User.
+router.post('/user/signin', user_controller.user_signin_post);
 
 // GET request to update a User.
 router.get('/user/:id/update', user_controller.user_update_get);
