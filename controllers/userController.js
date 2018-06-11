e5cc43a157896ad03eb2c3de419ef46df2ca4ba1
twumm <martint.mensah@gmail.com>
2018-06-11 22:20:41 +0000

    feat: Configure signin for GET and POST.

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,19 +32,17 @@ exports.user_signup_post = [
       username: req.body.username,
       password: req.body.password,
     });
-    console.log(user);
 
     if (!errors.isEmpty()) {
       // There are errors in the sign up form data.
-      res.render('user_form', { title: 'POST - Welcome! Enter details to continue', signupTitle: 'Error in sign up. Please try again', user: user, errors: errors });
+      res.render('user_form', { title: 'Welcome! Enter details to continue', signupError: 'Error in sign up. Please try again', user: user, errors: errors });
     } else {
       // No errors so create user in db.
       user.save(function(err) {
         if (err) { return next(err); }
         // Successful so redirect to user profile.
-        // req.session.userId = user._id;
+        req.session.userId = user._id;
         res.redirect(user.url);
-        console.log('got to the user save function')
       });
     }
   }
@@ -56,13 +54,47 @@ exports.user_signup_post = [
 
 // Display User create form on GET.
 exports.user_signin_get = function(req, res, next) {
-  res.send('NOT IMPLEMENTED: User signin get');
+  res.render('user_form', { title: 'Welcome! Enter details to continue' });
+  // res.send('NOT IMPLEMENTED: User signin get');
 }
 
 // Handle User create on POST.
-exports.user_signin_post = function(req, res, next) {
-  res.send('NOT IMPLEMENTED: User signin post');
-}
+exports.user_signin_post = [
+  // Validate user input from the form.
+  body('userDetail', 'Username or email required').isLength({ min: 1 }).trim(),
+  body('password', 'Password required').isLength({ min: 1 }),
+
+  // Sanitize fields using wildcats.
+  sanitizeBody('*'),
+
+  // Process request
+  (req, res, err) => {
+    // Save errors from validation, if any.
+    const errors = validationResult(req);
+
+    // Create user object with the data entered.
+    const user = new User({
+      userDetail: req.body.userDetail,
+      password: req.body.password
+    });
+
+    // Check if there are errors in the form values.
+    if (!errors.isEmpty()) {
+      // There are errors so render form with the values.
+      res.render('user_form', { title: 'Welcome! Enter details to continued', signinError: 'Error in sign up. Please try again', user: user, errors: errors })
+    } else {
+      User.authenticate(req.body.userDetail, req.body.password, function(error, user) {
+        console.log(req.body.userDetail, req.body.password);
+        if (error || !user) {
+          res.render('user_form', { title: 'Welcome! Enter details to continue', signinError: 'Wrong username, email or password', user: user })
+        } else {
+          req.session.userId = user._id;
+          res.redirect('/');
+        }
+      });
+    }
+  }
+];
 
 // Display User detail form on GET.
 exports.user_detail = function(req, res, next) {
