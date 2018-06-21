184889bdfad6a23284337cba3406218901d2fb66
twumm <martint.mensah@gmail.com>
2018-06-21 19:55:51 +0000

    feat: Setting things up for the default page .

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const quiz_controller = require('../controllers/quizController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// GET request to display quiz home.
+router.get('/', quiz_controller.quiz_display_get);
 
 // GET request to display quiz.
-router.get('/:id', quiz_controller.quiz_display_get);
+// router.get('/:id', quiz_controller.quiz_display_get);
 
 // POST request to display quiz.
-router.post('/:id', quiz_controller.quiz_display_post);
+// router.post('/:id', quiz_controller.quiz_display_post);
 
 module.exports = router;
