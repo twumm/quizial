9c66045671e1e11338ae58f9194a9154e3d425fb
twumm <martint.mensah@gmail.com>
2018-07-06 16:21:46 +0000

    feat: Create and save questions.

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -4,6 +4,7 @@ const Answer = require('../models/answer');
 
 const { body, validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
+const async = require('async');
 
 
 // Display Question create form on GET.
@@ -15,7 +16,51 @@ exports.question_create_get = function(req, res, next) {
 // Handle Question create on POST.
 exports.question_create_post = [
   // Validate input fields.
-  body('')
+  body('question', 'Question must not be empty').isLength({ min: 1 }).trim(),
+  body('correctOption', 'Correct answer must not be empty').isLength({ min: 1 }).trim(),
+  body('wrongOptionOne', 'Wrong answer must not be empty').isLength({ min: 1 }).trim(),
+  body('wrongOptionTwo', 'Wrong answer must not be empty').isLength({ min: 1 }).trim(),
+  body('wrongOptionThree', 'Wrong answer must not be empty').isLength({ min: 1 }).trim(),
+
+  // Sanitize values using wildcard.
+  sanitizeBody('*'),
+
+  // Process request.
+  (req, res, next) => {
+    // Store errors from validation, if any.
+    const errors = validationResult(req);
+
+    // Create answer variable for correct answer.
+    const answer = new Answer({
+      answerCorrect: req.body.correctOption,
+      answerOption: [req.body.correctOption, req.body.wrongOptionOne,
+        req.body.wrongOptionTwo, req.body.wrongOptionThree
+      ]
+    });
+
+    // Create question variable to contain question from the form.
+    const question = new Question({
+      question: req.body.question,
+      answer: answer._id
+    });
+
+    // If there are errors, render form with values.
+    if (!errors.isEmpty()) {
+      res.render('question_form', { title: 'Please check for errors', question: question, answers: answer, errors: errors });
+    } else {
+      async.series({
+        save_question: function(callback) {
+          question.save(callback);
+        },
+        save_answer: function(callback) {
+          answer.save(callback);
+        }
+      }, function(err, callback) {
+        if (err) { return next(err); }
+        return res.redirect('/');
+      })
+    }
+  }
 ]
 
 /*function(req, res, next) {
