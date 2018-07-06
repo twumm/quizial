8203fa1c99939595ad7cb28dca00ee53efe78eca
twumm <martint.mensah@gmail.com>
2018-07-06 18:51:54 +0000

    feat: Query all questions in the display and render in quiz display.

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -2,9 +2,20 @@ const Question = require('../models/question');
 const User = require('../models/user');
 const Answer = require('../models/answer');
 
+// Display quiz home on GET.
+exports.quiz_home_get = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: Quiz home page')
+}
+
 // Display quiz on GET.
 exports.quiz_display_get = function(req, res, next) {
-  res.send('NOT IMPLEMENTED: Quiz display get');
+  // res.send('NOT IMPLEMENTED: Quiz display get');
+  // Query for questions in the db.
+  Question.find()
+    .populate('answer')
+    .exec()
+    .then(questions => res.render('quiz_display', {title: 'Quiz here', questions: questions, user: req.user}))
+    .catch(err => res.join(err));
 }
 
 exports.quiz_display_post = function(req, res, next) {
