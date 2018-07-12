0e63e2a3425d4bed5674cb5bbe1de0b34420b77e
twumm <martint.mensah@gmail.com>
2018-07-12 07:18:40 +0000

    feat: Save questions attempted and questions answered correctly.

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -26,10 +26,7 @@ exports.quiz_display_get = function(req, res, next) {
           
           Question.findOne().skip(randomQuestionNum)
             .populate('answer')
-            // .limit(1)
             .exec(callback);
-            // .then(questions => res.render('quiz_display', {title: 'Quiz here', questions: questions, user: req.user}))
-            // .catch(err => res.join(err));
         })
     },
     user: callback => {
@@ -37,9 +34,11 @@ exports.quiz_display_get = function(req, res, next) {
         .exec(callback);
     }
   }, (err, results) => {
-    // console.log(results);
     if (err) { return next(err);}
     if (!results.user.questionsAttempted.includes(results.question._id)) {
+      // results.user.questionsAttempted.push(results.question._id);
+      // results.user.save();
+      console.log(results.question._id)
       res.render('quiz_display', { quiz: results.question, user: req.user})
     }
   })
@@ -51,6 +50,54 @@ exports.quiz_display_get = function(req, res, next) {
     .catch(err => res.join(err));*/
 }
 
+// Handle quiz response/display on POST.
 exports.quiz_response_post = function(req, res, next) {
-  res.send('NOT IMPLEMENTED: Quiz display post');
+  // Save the answer selected by the user.
+  let userAnswer = req.body.possibleAnswer;
+
+  // Query for user and question attempted.
+  async.parallel({
+    user: callback => {
+      User.findById(req.user)
+      .exec(callback)
+    },
+    question: callback => {
+      Question.findById(req.body.questionID)
+      .populate('answer')
+      .exec(callback)
+    }
+  }, (err, results) => {
+      if (err) { return next(err); }
+      console.log(results.question.answer.answerCorrect)
+      if (results.question.answer.answerCorrect !== userAnswer) {
+        results.user.questionsAttempted.push(results.question._id)
+        // results.user.questionsAttemptedCount++
+        /*results.user.save(
+          {$push: {questionsAttempted: results.question._id}},
+          {$inc: {questionsAttemptedCount: 1}})*/
+        results.user.save()
+        res.redirect('back')
+      }
+      else if (results.question.answer.answerCorrect === userAnswer) {
+        results.user.questionsCorrect.push(results.question._id)
+        // results.user.questionsAttemptedCount++
+        results.user.save()
+        res.redirect('back')
+      }
+  })
+  /*Question.findById(req.body.questionID)
+    .populate('answer')
+    .exec((err, question) => {
+      if (err) { return next(err);}
+      if (question.answer.answerCorrect !== userAnswer) {
+        res.redirect('back')
+      }
+      else if (question.answer.answerCorrect === userAnswer) {
+
+      }
+      console.log(question);
+    })*/
+  console.log(req.body.possibleAnswer)
+  console.log(req.body.questionID)
+  // res.redirect('back')
 }
