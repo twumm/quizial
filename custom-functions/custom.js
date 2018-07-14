c0a847370c2a79d796ef3d44a8b9594b277d0854
twumm <martint.mensah@gmail.com>
2018-07-14 20:59:48 +0000

    feat: Export function that checks if object exists in an array of objects.

--- custom-functions/custom.js
+++ custom-functions/custom.js
@@ -2,6 +2,6 @@ const _ = require('underscore')
 
 exports.containsObject = (object, list) => {
   let result = _.find(list, value => { return _.isEqual(object, value)})
-  console.log(_.isObject(result))
+  // console.log(_.isObject(result))
   return (_.isObject(result)) ? true:false
 }
