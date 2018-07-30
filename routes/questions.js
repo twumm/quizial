7bb28838032aa545002650664085c959d431be8b
twumm <martint.mensah@gmail.com>
2018-07-30 19:48:21 +0000

    feat: Require login before giving access to routes.

--- routes/questions.js
+++ routes/questions.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const question_controller = require('../controllers/questionController');
+const express = require('express')
+const questionController = require('../controllers/questionController')
+const { requiresLogin } = require('../middleware/custom')
 
-/// QUESTION ROUTES ///
+const router = express.Router()
+
+// / QUESTION ROUTES ///
 
 // GET request to create a Question.
-router.get('/question/create', question_controller.question_create_get);
+router.get('/question/create', requiresLogin, questionController.question_create_get)
 
 // POST request to create a Question.
-router.post('/question/create', question_controller.question_create_post);
+router.post('/question/create', requiresLogin, questionController.question_create_post)
 
 // GET request to update a Question.
-router.get('/question/:id/update', question_controller.question_update_get);
+router.get('/question/:id/update', requiresLogin, questionController.question_update_get)
 
 // POST request to update a Question.
-router.post('/question/:id/update', question_controller.question_update_post);
+router.post('/question/:id/update', requiresLogin, questionController.question_update_post)
 
 // GET request to delete a Question.
-router.get('/question/:id/delete', question_controller.question_delete_get);
+router.get('/question/:id/delete', requiresLogin, questionController.question_delete_get)
 
 // POST request to update a Question.
-router.post('/question/:id/delete', question_controller.question_delete_post);
+router.post('/question/:id/delete', requiresLogin, questionController.question_delete_post)
 
-module.exports = router;
+module.exports = router
